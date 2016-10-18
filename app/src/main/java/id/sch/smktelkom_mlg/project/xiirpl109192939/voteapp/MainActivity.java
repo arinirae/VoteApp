95b38e3d787dd1240fbc90ba9c33508119d5fb98
toshiba <gbagoescahyono@gmail.com>
2016-10-18 13:19:33 +0700

    set text

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl109192939/voteapp/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl109192939/voteapp/MainActivity.java
@@ -2,12 +2,19 @@ package id.sch.smktelkom_mlg.project.xiirpl109192939.voteapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        tvHasil.setText("gandhy");
     }
 }
