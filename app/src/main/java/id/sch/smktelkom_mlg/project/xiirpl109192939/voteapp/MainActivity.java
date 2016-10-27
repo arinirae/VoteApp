363e0d5efdccb5e3eabaa626d123d4e65ce4bda8
toshiba <gbagoescahyono@gmail.com>
2016-10-27 21:59:47 +0700

    register user

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl109192939/voteapp/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl109192939/voteapp/MainActivity.java
@@ -1,57 +1,99 @@
 package id.sch.smktelkom_mlg.project.xiirpl109192939.voteapp;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
-    TextView tvHasil;
-    public  VoteAPI vot = new VoteAPI();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Firebase.setAndroidContext(this);
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        setTitle("Register");
 
-        Button btn = (Button) findViewById(R.id.button);
+        //inisialisasi
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        //end inisialisasi
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvHasil.setText(vot.getChildData("email"));
-            }
-        });
-
-
+        //agar button dan textView bisa di click
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+        //end agar button dan textView bisa di click
     }
+
+    //method untuk daftar
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        //validasi form
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //end validasi form
+
+        //validasi form OK
+        progressDialog.setMessage("Registering user ...");
+        progressDialog.show();
+        //end validasi form OK
+
+        //penyimpanan data dalam firebase
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Could not register, please try again", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+        //end penyimpanan data dalam firebase
+    }
+    //end method untuk daftar
 
     @Override
-    protected void onStart(){
-        super.onStart();
-        vot.setRef("https://voteapp-e3557.firebaseio.com/user/");
-        vot.fetchData();
-        vot.fetchDataChild(vot.getKey(vot.findKeyIndex("0")));
-        tvHasil.setText(vot.getChildData("username"));
+    public void onClick(View view) {
+        if (view == buttonRegister) {
+            registerUser();
+        }
 
+        if (view == textViewSignin) {
+            //disini untuk membuka halaman login
+        }
 
     }
-
-
-
 }
 
